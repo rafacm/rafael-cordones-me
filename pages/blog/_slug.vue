a72d16a7d55d4f1bd355350c14f64086c49940ee
Rafael Cordones <rafael@cordones.me>
2020-10-07 17:32:22 +0200

    Improve blog article page

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,22 +1,19 @@
 <template>
-  <div
-    class="flex lg:h-screen w-screen lg:overflow-hidden xs:flex-col lg:flex-row"
-  >
-    <div class="relative lg:w-1/3 xs:w-full xs:h-84 lg:h-full post-left">
-      <content-image
-        class="absolute h-full w-full object-cover"
-        :dir="article.dir"
-        :src="article.image.path"
-        :alt="article.image.alt"
-      />
-    </div>
-    <article
-      class="prose relative xs:py-4 xs:px-8 lg:py-8 lg:px-16 lg:w-2/3 xs:w-full h-full overflow-y-scroll markdown-body post-right custom-scroll"
-    >
+  <div class="w-full mx-auto">
+    <article class="prose max-w-none lg:prose-lg xl:prose-xl">
       <h1 class="text-4xl font-bold">{{ article.title }}</h1>
       <h3 class="mt-2 -mb-3 flex uppercase text-sm">
         {{ formatDate(article.date) }}
       </h3>
+      <figure>
+        <content-image
+          class="w-full"
+          :dir="article.dir"
+          :src="article.image.path"
+          :alt="article.image.alt"
+        />
+        <figcaption v-html="article.image.caption"></figcaption>
+      </figure>
       <nuxt-content :document="article" />
       <!-- prevNext component -->
       <PrevNext :prev="prev" :next="next" class="mt-8" />
