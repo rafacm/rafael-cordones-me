4c7247e3fc9fb24b9440ea5506c4c4f0dfaf6fbe
Rafael Cordones <rafael@cordones.me>
2020-10-08 09:45:04 +0200

    Add CSS style to the body tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,10 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    bodyAttrs: {
+      class: 'text-gray-800 antialiased'
+    }
   },
   /*
    ** Global CSS
