32409a3136f72a8ea0a4601990892cab4503c9e8
Rafael Cordones <rafael@cordones.me>
2021-02-18 08:20:13 +0100

    Remove next/image use since it is not supported on AWS Amplify

--- src/components/blog-card.js
+++ src/components/blog-card.js
@@ -1,12 +1,11 @@
 import Link from 'next/link'
-import Image from 'next/image'
 
 const BlogCard = ({ post }) => {
   return (
     <div className="rounded overflow-hidden shadow-lg my-2">
       <Link as={`/blog/${post.slug}`} href="/blog/[slug]">
         <a aria-label={post.title}>
-          <Image className="w-full" src={post.image.path} alt="Sunset in the mountains" width="350" height="200" objectFit='cover'/>
+          <img className="w-full object-cover" src={post.image.path} alt="Sunset in the mountains" width="350" height="200"/>
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">{post.title}</div>
           </div>
